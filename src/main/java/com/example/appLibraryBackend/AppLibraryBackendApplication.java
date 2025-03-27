5281aba526f49b5c660f0672277dd6d474cdf15b
Robinson <robinquiroz57@gmail.com>
2025-03-26 19:25:17 -0500

    inicio app

---
+++ src/main/java/com/example/appLibraryBackend/AppLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appLibraryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appLibraryBackend/AppLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appLibraryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
